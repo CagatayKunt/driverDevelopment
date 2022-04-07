6af1689cf6b98c28f0fe3872a3befca8e28517a6
cagatay <cagataykunt@gmail.com>
2022-04-08 00:46:43 +0300

    bus domain macro defined

--- myDriver/Inc/stm32f407xx.h
+++ myDriver/Inc/stm32f407xx.h
@@ -15,5 +15,19 @@
 #define SRAM2_BASE_ADDR				(0x2001C000UL) /* SRAM2 Base Address 112 KB */
 
 
+/*
+ * Peripheral Base Addresses
+ *
+*/
+
+#define PERIPH_BASE_ADDR			(0x40000000UL)						/* Base Address for All peripherals		*/
+
+#define APB1_BASE_ADDR				PERIPH_BASE_ADDR					/* APB1 Bus Domain Base Address			*/
+#define APB2_BASE_ADDR				(PERIPH_BASE_ADDR + 0x00010000UL)	/* APB2 Bus Domain Base Address			*/
+#define AHB1_BASE_ADR				(PERIPH_BASE_ADDR + 0X00020000UL)	/* AHB1 Bus Domain Base Address			*/
+#define AHB2_BASE_ADR				(PERIPH_BASE_ADDR + 0X10000000UL)	/* AHB2 Bus Domain Base Address			*/
+
+
+
 
 #endif /* INC_STM32F407XX_H_ */
