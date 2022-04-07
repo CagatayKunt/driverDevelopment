689b8643d6fe48d97e3336f15960084a7684cf13
cagatay <cagataykunt@gmail.com>
2022-04-07 23:57:09 +0300

    firstCommit
    myDriver(Inc, Src) klasoru eklendi. Inc klasoru icine stm32f407xx.h eklendi. Memory macro tanimlamalari eklendi

---
+++ myDriver/Inc/stm32f407xx.h
@@ -0,0 +1,19 @@
+
+
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+
+
+/*
+ * Memory Base Address
+ *
+ */
+
+#define FLASH_BASE_ADDR				(0x08000000UL) /* Flash Base Address (up to 1 MB)	*/
+#define	SRAM1_BASE_ADDR				(0x20000000UL) /* SRAM1 Base Address 112 KB */
+#define SRAM2_BASE_ADDR				(0x2001C000UL) /* SRAM2 Base Address 112 KB */
+
+
+
+#endif /* INC_STM32F407XX_H_ */
